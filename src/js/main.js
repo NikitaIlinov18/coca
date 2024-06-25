16f9fea91ae143e3c2271acce3e7a8456701b385
NikitaIlinov18 <nikitailinov424@gmail.com>
2024-06-26 01:13:56 +0300

    Add footer section adapted for mobile

--- src/js/main.js
+++ src/js/main.js
@@ -6,8 +6,10 @@ import { useTheme } from './components/theme.js';
 import { useBurger } from './components/burger.js';
 import { useInsightSlider } from './components/home/slider.js';
 import { useTestimonialsSlider } from './components/home/slider.js';
+import { useAccordion } from './components/footer-accordion.js';
 
 useTheme();
 useBurger();
 useInsightSlider();
 useTestimonialsSlider();
+useAccordion();
