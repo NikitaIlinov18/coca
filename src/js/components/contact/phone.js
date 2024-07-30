8452b9e042186b77201a6629fe6bea7cd4eaaedf
NikitaIlinov18 <nikitailinov424@gmail.com>
2024-07-30 11:42:30 +0300

    Fixed validation and maps

--- src/js/components/contact/phone.js
+++ src/js/components/contact/phone.js
@@ -13,6 +13,8 @@ export const usePhone = () => {
         .catch(() => callback('us'));
     },
     utilsScript:
-      'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js', // just for formatting/placeholders etc
+      'https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/23.4.0/js/utils.js' ||
+      '/intl-tel-input/js/utils.js?1721468771539',
+    strictMode: true,
   });
 };
